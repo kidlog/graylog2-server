2ea0d94a2b3f7d6c653a07a1ce4c80462d946c6c
Kyle Knight <kyle@graylog.com>
2020-06-09 07:20:05 -0500

    starting font sizes

--- graylog2-web-interface/src/components/graylog/Panel.jsx
+++ graylog2-web-interface/src/components/graylog/Panel.jsx
@@ -50,7 +50,7 @@ const StyledPanel = styled(BootstrapPanel)(({ theme }) => css`
   > ${PanelHeading} {
     .panel-title,
     .panel-title h3 {
-      font-size: 16px;
+      font-size: ${theme.fonts.size.body}
     }
   }
 
@@ -87,7 +87,7 @@ const deprecatedVariantStyles = (hex, variant) => css(({ theme }) => {
 
       > .panel-title,
       > .panel-title > * {
-        font-size: 16px;
+        font-size: ${theme.fonts.size.body}
       }
 
       + .panel-collapse > .panel-body {
